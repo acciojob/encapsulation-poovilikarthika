95799abc76c2a735e82ef5ef049f02f41dc18a65
poovilikarthika <pooviliselvi.8502gmail.com>
2023-01-13 21:27:34 +0530

    encapsulation 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      RWOnly obj=new RWOnly();
+     // System.out.println(obj.name="poovili");
+      //error
+      // C:\Users\admin\IdeaProjects\encapsulation-poovilikarthika\src\main\java\com\driver\Main.java:6:29
+      //java: name has private access in com.driver.RWOnly
+
+      obj.setName("Poovili");
+      System.out.println(obj.getName());
+
+  }
 }
